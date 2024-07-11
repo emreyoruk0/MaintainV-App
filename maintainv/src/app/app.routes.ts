91036c53ebbff6b2394219e858ffe42721a6d4cd
emreyoruk0 <emreyoruk01@gmail.com>
2024-07-11 18:02:23 +0300

    navbar, footer and not-found component coded and designed. half of home component done

--- maintainv/src/app/app.routes.ts
+++ maintainv/src/app/app.routes.ts
@@ -1,3 +1,30 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent:
+        () => import('./components/layouts/layouts.component')
+        .then(c => c.LayoutsComponent),
+        children: [
+          {
+            path: '',
+            loadComponent:
+                () => import('./components/home/home.component')
+                .then(c => c.HomeComponent)
+          },
+          {
+            path: 'home',
+            loadComponent:
+                () => import('./components/home/home.component')
+                .then(c => c.HomeComponent)
+          }
+        ]
+  },
+  {
+    path: '**',
+    loadComponent:
+        () => import('./components/not-found/not-found.component')
+        .then(c => c.NotFoundComponent)
+  }
+];
