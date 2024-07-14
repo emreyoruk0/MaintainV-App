095892414e04c1b7894912af3dc8fea3286caa58
emreyoruk0 <emreyoruk01@gmail.com>
2024-07-14 11:50:16 +0300

    div tags replaced with html semantic tags

--- maintainv/src/app/components/layouts/navbar/navbar.component.ts
+++ maintainv/src/app/components/layouts/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, HostListener } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -13,11 +13,21 @@ export class NavbarComponent {
   isShow: boolean = false;
   topPosToStartShowing = 100;
 
+  constructor(
+    private _router: Router
+  ){}
+
+  goHomeAndTop(){
+    this._router.navigateByUrl('/')
+        .then(() => {
+        window.scrollTo(0, 0);
+        });
+  }
+
 
   @HostListener('window:scroll')
   checkScroll() {
     const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-
     if (scrollPosition >= this.topPosToStartShowing) {
       this.isShow = true;
     } else {
